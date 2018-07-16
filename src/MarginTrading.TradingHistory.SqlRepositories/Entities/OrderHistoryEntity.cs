4961b341930f16e1c4adf8baf9125a68697b0dae
Alexey Andreev <alexey.andreev@lykke.com>
2018-07-16 17:53:15 +0300

    MTC-145: fix testing issues

--- src/MarginTrading.TradingHistory.SqlRepositories/Entities/OrderHistoryEntity.cs
+++ src/MarginTrading.TradingHistory.SqlRepositories/Entities/OrderHistoryEntity.cs
@@ -57,7 +57,8 @@ namespace MarginTrading.TradingHistory.SqlRepositories.Entities
             ? new List<RelatedOrderInfo>()
             : RelatedOrderInfos.DeserializeJson<List<RelatedOrderInfo>>();
 
-        public OrderUpdateType UpdateType { get; set; }
+        OrderUpdateType IOrderHistory.UpdateType => UpdateType.ParseEnum<OrderUpdateType>();
+        public string UpdateType { get; set; }
         public string AdditionalInfo { get; set; }
 
         public string MatchedOrders { get; set; }
@@ -105,7 +106,7 @@ namespace MarginTrading.TradingHistory.SqlRepositories.Entities
                 RelatedOrderInfos = order.RelatedOrderInfos.ToJson(),
                 TradingConditionId = order.TradingConditionId,
                 MatchedOrders = order.MatchedOrders.ToJson(),
-                UpdateType = order.UpdateType,
+                UpdateType = order.UpdateType.ToString(),
                 AdditionalInfo = order.AdditionalInfo
             };
         }
