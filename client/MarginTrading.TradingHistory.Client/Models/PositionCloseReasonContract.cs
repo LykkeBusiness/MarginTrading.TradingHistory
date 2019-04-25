870cd1f1a45eec62018b1c4e7529c79b640fcaa5
morttred <ivan.kolev@sircleadvertising.com>
2019-04-25 23:46:50 +0300

    MTC-767: Added missing JsonConverter attribute to enum types.

--- client/MarginTrading.TradingHistory.Client/Models/PositionCloseReasonContract.cs
+++ client/MarginTrading.TradingHistory.Client/Models/PositionCloseReasonContract.cs
@@ -1,5 +1,9 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
 namespace MarginTrading.TradingHistory.Client.Models
 {
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum PositionCloseReasonContract
     {
         None,
