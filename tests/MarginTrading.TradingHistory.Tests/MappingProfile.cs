1cf74fc71abc993b1e13c77bd61f90dd03c83dbb
Dmitriy Inozemtsev <dmitry.inozemtcev@lykke-business.ch>
2022-10-06 14:54:35 +0300

    [LT-4169] Added automapper test

---
+++ tests/MarginTrading.TradingHistory.Tests/MappingProfile.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) 2019 Lykke Corp.
+// See the LICENSE file in the project root for more information.
+
+using AutoMapper;
+using MarginTrading.TradingHistory.AzureRepositories.Entities;
+using MarginTrading.TradingHistory.Client.Models;
+using MarginTrading.TradingHistory.Core;
+using MarginTrading.TradingHistory.Core.Domain;
+using DealContract = MarginTrading.Backend.Contracts.Positions.DealContract;
+
+namespace MarginTrading.TradingHistory.Tests
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<IDeal, DealContract>(MemberList.None);
+            CreateMap<IAggregatedDeal, AggregatedDealContract>(MemberList.None);
+            CreateMap<IPositionHistory, PositionHistoryEntity>(MemberList.None);
+        }
+    }
+}
