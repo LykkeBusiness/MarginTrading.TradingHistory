6ad2b4111be1d07c25665e2ad437861c6f8c5124
atarutin <andrey.tarutin@lykke-business.ch>
2023-12-21 12:28:26 +0300

    fix(LT-5139): add timeline comparer for position history events

---
+++ tests/MarginTrading.TradingHistory.Tests/PositionEventComparerByTimelineTestData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using MarginTrading.TradingHistory.Client;
+using MarginTrading.TradingHistory.Client.Models;
+
+namespace MarginTrading.TradingHistory.Tests
+{
+    public static class PositionEventComparerByTimelineTestData
+    {
+        public static IEnumerable<object[]> AllHistoryTypeComparisons()
+        {
+            yield return new object[] { PositionHistoryTypeContract.Open, PositionHistoryTypeContract.Close, PositionEventComparerByTimeline.FirstLessThanSecond };
+            yield return new object[] { PositionHistoryTypeContract.Open, PositionHistoryTypeContract.PartiallyClose, PositionEventComparerByTimeline.FirstLessThanSecond };
+            yield return new object[] { PositionHistoryTypeContract.PartiallyClose, PositionHistoryTypeContract.Close, PositionEventComparerByTimeline.FirstLessThanSecond };
+            yield return new object[] { PositionHistoryTypeContract.Close, PositionHistoryTypeContract.Open, PositionEventComparerByTimeline.FirstGreaterThanSecond };
+            yield return new object[] { PositionHistoryTypeContract.Close, PositionHistoryTypeContract.PartiallyClose, PositionEventComparerByTimeline.FirstGreaterThanSecond };
+            yield return new object[] { PositionHistoryTypeContract.PartiallyClose, PositionHistoryTypeContract.Open, PositionEventComparerByTimeline.FirstGreaterThanSecond };
+            yield return new object[] { PositionHistoryTypeContract.Close, PositionHistoryTypeContract.Close, PositionEventComparerByTimeline.FirstEqualToSecond };
+            yield return new object[] { PositionHistoryTypeContract.PartiallyClose, PositionHistoryTypeContract.PartiallyClose, PositionEventComparerByTimeline.FirstEqualToSecond };
+            yield return new object[] { PositionHistoryTypeContract.Open, PositionHistoryTypeContract.Open, PositionEventComparerByTimeline.FirstEqualToSecond };
+        }
+    }
+}
