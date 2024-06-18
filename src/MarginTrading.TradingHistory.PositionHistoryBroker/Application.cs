eeeb8c6162c48b9a5b22e85f9887780779d31697
atarutin <andrey.tarutin@lykke-business.ch>
2024-06-18 12:47:27 +0300

    style(LT-5549): update editorconfig

--- src/MarginTrading.TradingHistory.PositionHistoryBroker/Application.cs
+++ src/MarginTrading.TradingHistory.PositionHistoryBroker/Application.cs
@@ -2,14 +2,17 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Threading.Tasks;
+
 using Lykke.MarginTrading.BrokerBase;
 using Lykke.MarginTrading.BrokerBase.Settings;
 using Lykke.Snow.Common.Correlation;
 using Lykke.Snow.Common.Correlation.RabbitMq;
+
 using MarginTrading.Backend.Contracts.Events;
 using MarginTrading.TradingHistory.Core.Domain;
 using MarginTrading.TradingHistory.Core.Repositories;
 using MarginTrading.TradingHistory.DapperExtensions;
+
 using Microsoft.Extensions.Logging;
 
 namespace MarginTrading.TradingHistory.PositionHistoryBroker
@@ -29,12 +32,16 @@ namespace MarginTrading.TradingHistory.PositionHistoryBroker
         public Application(
             CorrelationContextAccessor correlationContextAccessor,
             RabbitMqCorrelationManager correlationManager,
-            ILoggerFactory loggerFactory, 
-            IPositionsHistoryRepository positionsHistoryRepository, 
-            Settings settings, 
+            ILoggerFactory loggerFactory,
+            IPositionsHistoryRepository positionsHistoryRepository,
+            Settings settings,
             CurrentApplicationInfo applicationInfo,
             ILogger<Application> logger)
-            : base(correlationManager, loggerFactory, logger, applicationInfo)
+            : base(
+                correlationManager,
+                loggerFactory,
+                logger,
+                applicationInfo)
         {
             _correlationContextAccessor = correlationContextAccessor;
             _positionsHistoryRepository = positionsHistoryRepository;
@@ -54,17 +61,17 @@ namespace MarginTrading.TradingHistory.PositionHistoryBroker
             {
                 _logger.LogDebug($"Correlation id is empty for postition {positionHistoryEvent.PositionSnapshot.Id}");
             }
-            
+
             var position = positionHistoryEvent
                 .ToDomain()
                 .AddCorrelationId(correlationId);
-            
+
             var deal = (positionHistoryEvent.EventType == PositionHistoryTypeContract.Close
                         || positionHistoryEvent.EventType == PositionHistoryTypeContract.PartiallyClose)
                        && positionHistoryEvent.Deal != null
                 ? Deal.FromPositionHistoryEvent(positionHistoryEvent, correlationId)
                 : null;
-            
+
             await _positionsHistoryRepository.AddAsync(position, deal);
         }
     }
